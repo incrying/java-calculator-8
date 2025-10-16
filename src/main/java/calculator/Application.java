45d8c3ae6a08d9229a6174015fd312f35a7e9ee7
인다운 <incrying@duksung.ac.kr>
2025-10-17 00:20:18 +0900

    feat(Application): 메인 기능 작성

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,7 +1,22 @@
 package calculator;
 
+import camp.nextstep.edu.missionutils.Console;
+
+import java.util.List;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        System.out.println("덧셈할 문자열을 입력해 주세요.");
+        String input = Console.readLine();
+
+        DelimeterManager delimeterManager = new DelimeterManager();
+        NumberManager numberManager = new NumberManager();
+
+        String delimeter = delimeterManager.DelimeterFinder(input);
+        String subString = StringParser.splitString(input);
+        List<Integer> numbers = numberManager.numberExtractor(subString,delimeter);
+        int result = Adder.add(numbers);
+
+        System.out.println("결과 : " + result);
     }
 }
