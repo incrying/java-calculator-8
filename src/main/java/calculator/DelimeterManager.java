10b4b3ffe8978890b6012219e76f245e5d629f13
인다운 <incrying@duksung.ac.kr>
2025-10-18 01:52:09 +0900

    refactor: DelimeterManager 구조 수정

--- src/main/java/calculator/DelimeterManager.java
+++ src/main/java/calculator/DelimeterManager.java
@@ -8,20 +8,29 @@ public class DelimeterManager {
     private String subString;
 
     public String getDelimeter(String input) {
-        if (input.startsWith("//") && input.contains("\\n")) {
-            subString = input.substring(2, input.indexOf("\\n"));
-            if (subString.isBlank())
-                return delimeter;
-            if(subString.length() > 1)
-                throw new IllegalArgumentException("구분자는 여러 개일 수 없습니다.");
-            if(Character.isDigit(subString.charAt(0)))
-                throw new IllegalArgumentException("구분자는 숫자일 수 없습니다.");
-            if (isSpecialCharacter(subString))
-                subString = "\\" + subString;
-            delimeter += "|" + subString;
+        // 기본 구분자 반환
+        if (!input.startsWith("//") || !input.contains("\\n"))
             return delimeter;
-        } else
+
+        subString = input.substring(2, input.indexOf("\\n"));
+        if (subString.isBlank())
             return delimeter;
+
+        validateSubString(subString);
+
+        if (isSpecialCharacter(subString))
+            subString = "\\" + subString;
+
+        return delimeter + "|" + subString;
+    }
+
+    private void validateSubString(String subString) {
+        if (subString.length() > 1) {
+            throw new IllegalArgumentException("구분자는 여러 개일 수 없습니다.");
+        }
+        if (Character.isDigit(subString.charAt(0))) {
+            throw new IllegalArgumentException("구분자는 숫자일 수 없습니다.");
+        }
     }
 
     private boolean isSpecialCharacter(String input) {
